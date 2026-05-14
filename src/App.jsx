92ac834ef5cbb7d1250f417eaa0e7f990c453c8a
Rakibislam22 <rakibislam13122018@gmail.com>
2026-05-15 00:54:35 +0600

    feat: add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-5xl text-center text-blue-400">Hi this is a Hintro Dashboard</h1>
+      {/* <h1 className="text-5xl text-center text-blue-400">Hi this is a Hintro Dashboard</h1> */}
+      <Login />
     </div>
   );
 };
---
+++ src/components/Login.jsx
@@ -0,0 +1,76 @@
+
+const Login = () => {
+    return (
+        <div className="w-full flex items-center justify-center lg:h-screen max-sm:py-16">
+            <div className="w-sm">
+                <h2 className="text-3xl font-bold mb-14 text-center">Login</h2>
+                <form>
+                    <div className="mb-8">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            Email
+                        </label>
+                        <label className="input validator h-12 w-full rounded-lg">
+                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <g
+                                    strokeLinejoin="round"
+                                    strokeLinecap="round"
+                                    strokeWidth="2.5"
+                                    fill="none"
+                                    stroke="currentColor"
+                                >
+                                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                                </g>
+                            </svg>
+                            <input type="email" placeholder="mail@site.com" required />
+                        </label>
+                        <div className="validator-hint hidden">Enter valid email address</div>
+                    </div>
+                    <div className="mb-10">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <label className="input validator h-12 w-full rounded-lg">
+                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <g
+                                    strokeLinejoin="round"
+                                    strokeLinecap="round"
+                                    strokeWidth="2.5"
+                                    fill="none"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+                                    ></path>
+                                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                                </g>
+                            </svg>
+                            <input
+                                type="password"
+                                required
+                                placeholder="Password"
+                                minLength="8"
+                                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                            />
+                        </label>
+                        <p className="validator-hint hidden">
+                            Must be more than 8 characters, including
+                            <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+                        </p>
+                    </div>
+                    <div>
+                        <button
+                            className="bg-primary hover:bg-secondary w-full h-12 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+                            type="submit"
+                        >
+                            Sign In
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
